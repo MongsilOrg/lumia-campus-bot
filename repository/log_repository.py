5991ef4238c17031bdedaad9d680b5ec99079412
hello <yup299@gmail.com>
2026-03-11 10:07:21 +0900

    fix : log 데이터의 리턴 형식을 수정하였습니다.

--- repository/log_repository.py
+++ repository/log_repository.py
@@ -1,7 +1,7 @@
 from service.superbase import supabase
 
 # 자신의 구매 내역 조회
-# 반환 형식 {'store_name': '루미아 캠퍼스 아이콘', 'store_product': 'jadas82211'}
+# 반환 형식 [루미아 캠퍼스 아이콘', jadas82211']
 def get_log(user_id):
     response = (
         supabase.table("store")
@@ -10,4 +10,4 @@ def get_log(user_id):
         .execute()
     )
     res = response.data
-    return res[0] if res else None
+    return [res[0]['store_name'],res[0]['store_product']] if res else None
