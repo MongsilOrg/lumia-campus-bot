65e276466d36c65d5e375d7406eed10134462e04
Mongsil <mail@mongsil.dev>
2026-03-13 23:39:52 +0900

    fix: 유저 repository 응답 데이터 체크 수정 및 이메일 선택사항 변경

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,19 +1,20 @@
 from service.superbase import supabase
 
-# 임시 사용자 추가(이메일이 필수)
+# 사용자 추가
 # return id : int
-async def add_user(discord_id, name , email , point = 0):
+async def add_user(discord_id, name , email = None, point = 0):
     data = {
         "user_name" : name,
         "user_id" : discord_id,
         "user_point" : point,
-        "user_email" : email,
     }
+    if email is not None:
+        data["user_email"] = email
     res = await supabase.table("users").insert(data).execute()
-    return res.data[0]['id'] if res else None
+    return res.data[0]['id'] if res.data else None
 
 # 사용자 삭제
 # return user_name : str
 async def delete_user(user_id):
     res = await supabase.table("users").delete().eq("user_id", user_id).execute()
-    return res.data[0]['user_name'] if res else None
+    return res.data[0]['user_name'] if res.data else None
